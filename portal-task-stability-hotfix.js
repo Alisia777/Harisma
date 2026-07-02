dde4ad8bd1dc8160c498fa2153e68538f3ffb8aa
Alisia777 <artiukhins@gmail.com>
2026-07-02 09:10:10 +0300

    fix(tasks): add stability hotfix placeholder

---
+++ portal-task-stability-hotfix.js
@@ -0,0 +1,6 @@
+(function(){
+  'use strict';
+  if (window.__ALTEA_TASK_STABILITY_HOTFIX__) return;
+  window.__ALTEA_TASK_STABILITY_HOTFIX__ = true;
+  console.info('[task-stability-hotfix] loaded');
+})();
